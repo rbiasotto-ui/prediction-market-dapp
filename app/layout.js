831b95ef9301b42386f61793a9723cd5a1bd668a
rbiasotto-ui <rbiasotto@gmail.com>
2025-12-10 17:15:25 -0300

    layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
-export const metadata = { title: "Prediction Market DApp" };
+import './globals.css';
+import ConnectWallet from '../components/ConnectWallet';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ConnectWallet />
+        {children}
+      </body>
     </html>
   );
 }
